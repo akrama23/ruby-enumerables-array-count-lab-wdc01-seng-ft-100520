b2d071cdec31f61c2a0e3bc98fc659bb5b211d29
akam alam <arundelfootball23@gmail.com>
2020-09-28 00:30:15 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -2,8 +2,8 @@ def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
 
 array.count do |element|
- if  element == String 
- end 
+  element == String 
+ 
 end 
 
 
